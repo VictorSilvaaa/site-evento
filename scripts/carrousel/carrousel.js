3cdea93a6868600c50f36c3ed7cf745f6652112b
João Victor da Silva Sales <joao60651@gmail.com>
2022-08-10 09:14:46 -0300

    home/about/timeline/footer

---
+++ scripts/carrousel/carrousel.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper('.swiper', {
+    
+    direction: 'horizontal',
+    loop: true,
+    sliderPerView: 1,
+       
+    pagination: {
+        el: '.swiper-pagination',
+      },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
